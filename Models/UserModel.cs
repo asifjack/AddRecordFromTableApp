cc50f9eae4e001a531fc625741479fa7b2f04077
admin <admin@DESKTOP-8GOT5CO>
2023-05-24 04:47:40 +0530

    Add project files.

---
+++ Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AddRecordFromTableApp.Models
+{
+    public class UserModel
+    {
+
+       public int Id { get; set; }
+       public string? Email { get; set; }
+       public string? MSG { get; set; }
+        
+    }
+}
